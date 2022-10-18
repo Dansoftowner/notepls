7bd6831672762bb7b85d2ad9829f36a0c9296a28
Daniel Gyorffy <judalion777@gmail.com>
2022-10-18 13:17:27 +0200

    :tada: Project setup

---
+++ src/main/kotlin/com/dansoftware/notepls/NoteplsApplication.kt
@@ -0,0 +1,11 @@
+package com.dansoftware.notepls
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NoteplsApplication
+
+fun main(args: Array<String>) {
+	runApplication<NoteplsApplication>(*args)
+}
---
+++ src/test/kotlin/com/dansoftware/notepls/NoteplsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dansoftware.notepls
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NoteplsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
