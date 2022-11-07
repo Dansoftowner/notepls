7a88c663c5cb69e3f41539dcc44c82c0ac4bba28
Daniel Gyorffy <judalion777@gmail.com>
2022-11-07 13:17:11 +0100

    :construction: Improving note editing

--- src/main/resources/static/js/editor.js
+++ src/main/resources/static/js/editor.js
@@ -1,6 +1,24 @@
 const textArea = document.getElementById("raw-note-area");
 const notePreview = document.getElementById("note-preview");
 
+document.getElementById("bold-btn").addEventListener("click", () => {
+    let value = textArea.value;
+    let length = textArea.value.length;
+    let startIndex = textArea.selectionStart;
+    let endIndex = textArea.selectionEnd;
+    let selected = textArea.value.substring(startIndex, endIndex);
+    textArea.value = value.substring(0, startIndex) + `*${selected}*` + value.substring(endIndex);
+});
+document.getElementById("italic-btn").addEventListener("click", () => {
+
+});
+document.getElementById("underline-btn").addEventListener("click", () => {
+
+});
+document.getElementById("strikethrough-btn").addEventListener("click", () => {
+
+});
+
 function init() {
     textArea.addEventListener("onchange", updateNotePreview);
     textArea.addEventListener("keypress", updateNotePreview);
